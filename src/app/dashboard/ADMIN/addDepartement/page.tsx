61bb03d09b1904773c45809b90aa6fb0906fd19e
root <root@vps101055.serveur-vps.net>
2025-12-13 12:20:57 +0100

    update

--- src/app/dashboard/ADMIN/addDepartement/page.tsx
+++ src/app/dashboard/ADMIN/addDepartement/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { Header } from "@/components/Layouts/header";
+
 import { Sidebar } from "@/components/Layouts/sidebar";
 import { formElements } from "@/components/FormElements/forms";
 import Link from "next/link"
-
-import {useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { controllers } from "@/app/main"
+import { urlAPI } from "@/app/main";
+import {FormEvent ,useEffect, useState } from "react";
 
 
 type input = {
@@ -20,7 +23,9 @@ type input = {
 export default function AddDepartment(){
 
     const [getEnterpriseId, setGetEnterpriseId] = useState<any[]>([])
-    const [inputs, setInputs] = useState({
+    const [getEnterpriseIdOfAdmin, setGetEnterpriseIdOfAdmin] = useState<string | null>(null)
+    const [getAdminRole, setGetAdminRole] = useState<string | null>(null)
+    const [inputs, setInputs] = useState<input>({
       name: null,
       description:  null,
       enterpriseId: null,
@@ -29,6 +34,68 @@ export default function AddDepartment(){
     })
 
     //Récupération des entreprise
+    useEffect(() =>{
+        (async () => {
+
+            const authToken = await localStorage.getItem("authToken")
+            const role = await localStorage.getItem("role")
+            const getEnterpriseIdOfAdmin = await localStorage.getItem("EnterpriseId")
+
+            setGetEnterpriseIdOfAdmin(getEnterpriseIdOfAdmin)
+            setGetAdminRole(role)
+
+            const methodName = "getDepartmentPost"
+
+            const getEnterprise = await controllers.API.getAll(urlAPI, methodName, null)
+
+            if(parseInt(getEnterpriseIdOfAdmin ?? "") !== 1){
+            
+                const filteredIdOfAdmin = getEnterprise.filter((item : {id: number}) => item.id === parseInt(getEnterpriseIdOfAdmin ?? "" ))
+
+                setGetEnterpriseId(filteredIdOfAdmin)
+                return;
+
+            }
+            setGetEnterpriseId(getEnterpriseId)
+            console.log("l'id de l'entreprise", getEnterpriseId)
+               
+                
+        })()
+    }, [] )
+
+    let arrayData = [
+        {
+            alias: "EnterpriseId",
+            value: getEnterpriseId.filter(item => item.id === item.name).map(item => (
+                {id: item.id, value: item.name}
+            ))
+        }
+    ]
+
+    const handleSubmit = async (e: FormEvent) => {
+
+        const datas = {
+            name : inputs.name,
+            description: inputs.description,
+            enterpriseId: inputs.enterpriseId,
+            enterprise: inputs.enterprise
+        }
+
+        console.log("les données:", datas)
+
+        const methodName = "creatDepartmentPost"
+        const response = await controllers.API.SendOne(urlAPI, methodName, null)
+        
+       controllers.alertMessage(
+        response.status,
+        response.title,
+        response.message,
+        response.status ? "/dashboard/ADMIN/addDepartment" : null
+       )
+
+       
+
+    }
    
 
     return (
@@ -54,7 +121,86 @@ export default function AddDepartment(){
                                 </div>
                                 
                                 <hr className="bg-gray-400 h-[1px] boder-0 " />
-                                <div className="flex flex-wrap itemq-center space-y-4 lg:space-x-4">
+                                <div className="flex flex-wrap py-4 itemq-center space-y-4 lg:space-x-4">
+
+                                    {
+                                        element.addOrUpdateUser.navigationLinks.map((element, index) => (
+
+                                            <Link href={element.href} className={index === 0 ? "bg-blue-800 hover:bg-blue-900 ease duration-500 py-2 px-4 rounded relative top-2.5" : index === 5 ? "bg-blue-800 2xl:right-4 hover:bg-blue-900 ease duration-500 py-2 px-4 rounded relative 2xl:top-2.5 " : 
+                                                "bg-blue-800 hover:bg-blue-900 ease duration-500 py-2 px-4 rounded"} >
+                                                <FontAwesomeIcon icon={element.icon} className="text-white" /> <span className='text-white'>{element.title}</span>
+
+                                            </Link>
+                                            
+                                        ))
+                                    }
+
+                                    <div className='dark:border mt-8 w-full font-semibold h-auto border-gray-400 dark:border-gray-300 
+                                      rounded-[30px] border  dark:shadow-none p-4'>
+
+                                    </div>
+
+                                    {
+                                        formElements.map((item) => (
+
+                                            <div className="flex flex-wrap space-y-4 justify-between mb-2 items-center dark:text-gray-300 text-gray-700">
+                                                <h2 className="font-bold">  </h2>
+                                                <p className="font-semibold"> <span className="text-red-600">*</span> Champs obligatoires</p> 
+
+                                            </div>
+
+                                        ))
+                                    }
+
+                                    <hr className='bg-gray-400 border-0 h-[1px]' />
+                                    <div className='grid grid-cols-1 mt-4 gap-x-4 md:grid-cols-2 xl:grid-cols-3 font-semibold w-full'>
+                                        {
+                                            formElements.map((item) => (
+                                                 element.addOrUpdateUser.inputs.map((e, index) => (
+                                                    <div key={index} className="w-full mb-4">
+                                                        <label className="mb-4 font-semibold dark:text-gray-300 text-gray-700">
+                                                            <span className={e.requireField ? "text-red-600" : "hidden"} >*</span>
+                                                            {e.label}
+
+                                                        </label>
+
+                                                        {
+                                                           !e.selectedInput ? 
+                                                           <input onChange={(v) => {
+                                                            setInputs({
+                                                                ...inputs,
+                                                                [e.alias]: v.target.value
+                                                            })
+
+                                                           }} className="w-full mt-1 outline-none rounded-md dark:shadow-none 
+                                                           p-2.5 bg-transparent border-border-gray-400 dark:border-gray-300 dark:placeholder-gray-300 text-gray-700 dark:text-gray-300" 
+                                                           type={e.type} placeholder={e.placeholder} />
+                                                           :
+                                                           <select onChange={(u) => {
+                                                            setInputs({
+                                                                ...inputs,
+                                                                [e.alias]: u.target.value
+
+                                                            })
+                                                           }} className="w-full mt-1 outline-none rounded-md  dark:shadow-none p-2.5 bg-transparent 
+                                                               border border-gray-400 dark:border-gray-300 dark:bg-gray-900 font-normal dark:placeholder-gray-300 dark:text-gray-300 text-gray-700" >
+
+                                                                <option selected desabled >
+                                                                    {e.placeholder}
+                                                                </option>
+
+                                                           </select>
+                                                        }
+
+                                                    </div>
+
+                                                ))
+
+                                            ))
+                                        }
+
+                                    </div>
+                                    
                                     
                                    
 
